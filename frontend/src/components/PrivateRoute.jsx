bb695c39a0d81320397764bb7032ebb2a4bfdaec
Lokendra <lokendrarajpurohit01@gmail.com>
2025-06-10 11:18:22 +0500

    save

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -4,14 +4,14 @@ import { useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { addUser, removeUser } from "../store/features/authSlice";
 import { handleFailure } from "../utils/toast";
-import { Triangle } from "react-loader-spinner";
+import { RingLoader } from "react-spinners";
 
 const PrivateRoute = ({ children }) => {
   const [auth, setAuth] = useState(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("https://apnanotes-cdn4.onrender.com/check-auth", {
+    fetch("http://localhost:3000/check-auth", {
       method: "GET",
       credentials: "include", // 🧠 required to send cookie
     })
@@ -33,20 +33,7 @@ const PrivateRoute = ({ children }) => {
       });
   }, []);
 
-  if (auth === null)
-    return (
-      <div className="flex items-center justify-center h-[88dvh]">
-        <Triangle
-          visible={true}
-          height="80"
-          width="80"
-          color="#AC74FF"
-          ariaLabel="triangle-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-        />
-      </div>
-    );
+  if (auth === null) return <RingLoader color="#ac74ff" />;
   if (!auth) return <Navigate to="/user/login" replace />;
 
   return children;
